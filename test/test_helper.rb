002c1e3f7782b4ebba96027aebb743581bb48a24
minori-tuchimoti <189940358+minori-tuchimoti@users.noreply.github.com>
2025-06-07 15:20:05 +0000

    エラー修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,26 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "devise"  # ← これを追加
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    include Devise::Test::IntegrationHelpers  # ← Deviseのテストヘルパーを読み込む
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    parallelize(workers: :number_of_processors)
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    # fixtureのパスワードは平文で書いてあるので、テスト実行時に
+    # encrypted_passwordが空ならパスワードをセットして保存する
+    setup do
+      User.all.each do |user|
+        if user.encrypted_password.blank?
+          user.password = 'password'
+          user.save!
+        end
+      end
+    end
+
+    # 他に共通メソッドがあればここに書く
   end
 end

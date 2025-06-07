45f3d11e139801333fe23316680fbbbee198692e
minori-tuchimoti <189940358+minori-tuchimoti@users.noreply.github.com>
2025-06-07 16:47:18 +0000

    エラー修正

--- test/controllers/coffees_controller_test.rb
+++ test/controllers/coffees_controller_test.rb
@@ -1,23 +1,21 @@
-require "test_helper"
+test "should get index" do
+  get coffeeshops_url
+  assert_response :success
+end
 
-class CoffeeshopsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get coffeeshops_index_url
-    assert_response :success
-  end
+test "should get new" do
+  get new_coffeeshop_url
+  assert_response :success
+end
 
-  test "should get show" do
-    get coffeeshops_show_url
-    assert_response :success
-  end
+test "should get show" do
+  coffeeshop = coffeeshops(:one)  # fixtureなどで準備している場合
+  get coffeeshop_url(coffeeshop)
+  assert_response :success
+end
 
-  test "should get new" do
-    get coffeeshops_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get coffeeshops_edit_url
-    assert_response :success
-  end
+test "should get edit" do
+  coffeeshop = coffeeshops(:one)
+  get edit_coffeeshop_url(coffeeshop)
+  assert_response :success
 end

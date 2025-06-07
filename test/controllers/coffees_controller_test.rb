b63f049d8dac5ac25936f5ad81ee537f9513a615
minori-tuchimoti <189940358+minori-tuchimoti@users.noreply.github.com>
2025-06-07 17:18:39 +0000

    エラー修正

--- test/controllers/coffees_controller_test.rb
+++ test/controllers/coffees_controller_test.rb
@@ -1,25 +1,28 @@
 require "test_helper"
 
 class CoffeesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @headers = { "Accept" => "text/html" }
+    @coffee = coffees(:one)
+  end
+
   test "should get index" do
-    get coffees_url
+    get coffees_url, headers: @headers
     assert_response :success
   end
 
   test "should get new" do
-    get new_coffee_url
+    get new_coffee_url, headers: @headers
     assert_response :success
   end
 
   test "should get show" do
-    coffee = coffees(:one)
-    get coffee_url(coffee)
+    get coffee_url(@coffee), headers: @headers
     assert_response :success
   end
 
   test "should get edit" do
-    coffee = coffees(:one)
-    get edit_coffee_url(coffee)
+    get edit_coffee_url(@coffee), headers: @headers
     assert_response :success
   end
 end
